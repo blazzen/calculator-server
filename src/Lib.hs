504ff51de6b84bf6d05a60f71c828ab1efd5d661
blazzen <pavel.zhukov.official@gmail.com>
2020-12-19 22:30:50 +0300

    Add div, mul, sqrt and pow endpoints

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,6 +23,10 @@ $(deriveJSON defaultOptions ''CalcResponse)
 
 type API = "add" :> Capture "x" Double :> Capture "y" Double :> Get '[JSON] CalcResponse
       :<|> "sub" :> Capture "x" Double :> Capture "y" Double :> Get '[JSON] CalcResponse
+      :<|> "mul" :> Capture "x" Double :> Capture "y" Double :> Get '[JSON] CalcResponse
+      :<|> "div" :> Capture "x" Double :> Capture "y" Double :> Get '[JSON] CalcResponse
+      :<|> "sqrt" :> Capture "x" Double :> Get '[JSON] CalcResponse
+      :<|> "pow" :> Capture "x" Double :> Capture "n" Int :> Get '[JSON] CalcResponse
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -36,9 +40,25 @@ api = Proxy
 server :: Server API
 server = add
     :<|> sub
+    :<|> mul
+    :<|> Lib.div
+    :<|> Lib.sqrt
+    :<|> pow
 
 add :: Double -> Double -> Handler CalcResponse
 add x y = return (CalcResponse "add" [x, y] (x + y) Nothing)
 
 sub :: Double -> Double -> Handler CalcResponse
 sub x y = return (CalcResponse "sub" [x, y] (x - y) Nothing)
+
+mul :: Double -> Double -> Handler CalcResponse
+mul x y = return (CalcResponse "mul" [x, y] (x * y) Nothing)
+
+div :: Double -> Double -> Handler CalcResponse
+div x y = return (CalcResponse "div" [x, y] (x / y) Nothing)
+
+sqrt :: Double -> Handler CalcResponse
+sqrt x = return (CalcResponse "sqrt" [x] (Prelude.sqrt x) Nothing)
+
+pow :: Double -> Int -> Handler CalcResponse
+pow x n = return (CalcResponse "pow" [x, fromIntegral n] (x ** fromIntegral n) Nothing)
